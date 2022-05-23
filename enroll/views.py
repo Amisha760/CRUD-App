657017a932869a597b2cec04fddc4c3046f5f275
Amisha760 <amisha1505.cse19@chitkara.edu.in>
2022-05-23 23:05:40 +0530

    first commit

--- enroll/views.py
+++ enroll/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("hi")
+def add_show(request):
+    return render(request,'enroll/addandshow.html')
